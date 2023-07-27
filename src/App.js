ed071c4d80f777bc11847b8f720720e7b7b21276
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-27 21:46:10 +0500

    input states added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [login, setLogin]=useState(true)
+
+  const [email, setEmail]=useState('')
+  const [password, setPassword]=useState('')
+  const [country, setCountry]=useState('')
+  const [username, setUsername]=useState('')
+
+  const handleEmailChange=(event)=>{
+    setEmail(event.target.value)
+  }
+
+  const handlePasswordChange=(event)=>{
+    setPassword(event.target.value)
+  }
+
+  const handleCountryChange=(event)=>{
+    setCountry(event.target.value)
+  }
+
+  const handleUsernameChange=(event)=>{
+    setUsername(event.target.value)
+  }
+
+  const handleClick=()=>{
+    setLogin(!login)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+          {login ? 
+
+            <div className='container'>
+         <form className='signup-form'>
+            <div>
+              <label>Email: </label>
+              <input type='email' className='email' value={email} onChange={handleEmailChange}/>
+            </div>
+
+            <div>
+              <label>Password: </label>
+              <input type='password'className='password' value={password} onChange={handlePasswordChange}/>
+            </div>
+
+            <button>
+              Login
+            </button>
+
+            <button onClick={handleClick}>Click here to signup</button>
+            
+          </form>
+            </div>
+
+            : 
+
+            <div className='container'>
+
+              <form className='login-form'>
+              <div>
+                <label>Email: </label>
+                <input type='email' className='email' value={email} onChange={handleEmailChange}/>
+              </div>
+
+              <div>
+                <label>Username: </label>
+                <input type='text' className='email' value={username} onChange={handleUsernameChange}/>
+              </div>
+
+              <div>
+                <label>Country: </label>
+                <input type='text' className='email' value={country} onChange={handleCountryChange}/>
+              </div>
+
+              <div>
+                <label>Password: </label>
+                <input type='password' className='password' value={password} onChange={handlePasswordChange} />
+              </div>
+
+              <button>
+                Signup
+              </button>
+
+              <button onClick={handleClick}>Click here to login</button>
+              
+            </form> 
+            </div>}
     </div>
   );
 }
